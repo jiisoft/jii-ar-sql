1643ab6ecd8aa65b6c5608140d397407b4a9ff61
Kozhin Vladimir <affka@affka.ru>
2015-12-16 22:59:47 +0700

    remote client support (begin)

---
+++ index-client.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('jii');
+require('jii-model');
+
+// Load framework files
+require('./lib/ActiveRecord');
+require('./lib/Expression');
+require('./lib/remote/Connection');
+require('./lib/remote/Schema');
