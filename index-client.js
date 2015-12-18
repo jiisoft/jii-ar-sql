1bbb3426ca2c15d687b80087cddd57bca02145ad
Kozhin Vladimir <affka@affka.ru>
2015-12-18 23:49:48 +0700

    insert/update/delete model from client (testing..)

--- index-client.js
+++ index-client.js
@@ -6,5 +6,8 @@ require('jii-model');
 // Load framework files
 require('./lib/ActiveRecord');
 require('./lib/Expression');
+require('./lib/AfterSaveEvent');
+require('./lib/remote/Command');
 require('./lib/remote/Connection');
 require('./lib/remote/Schema');
+require('./lib/remote/TransportInterface');
